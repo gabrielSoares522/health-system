a380effa865a13c5c2210642f6f4af8505509af1
Gabriel Soares <gabrielcsoares52@gmail.com>
2021-06-27 12:01:10 -0300

    criando modelo

---
+++ health-system/Models/Diagnostico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace health_system.Models
+{
+    public class Diagnostico
+    {
+        public int DoencaId { get; set; }
+        public Doenca Doenca { get; set; }
+        public int ConsultaId { get; set; }
+        public Consulta Consulta { get; set; }
+    }
+}
